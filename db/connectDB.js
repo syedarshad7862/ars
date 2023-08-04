d38d0867c2b4ff241bb834bba04c06a4ba068239
Arshad <sa0675878@gmail.com>
2023-08-04 18:16:45 +0530

    added mongodb link

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 const connectDB = async() =>{
     try{
-        await mongoose.connect('mongodb://127.0.0.1:27017/portfolio');
-      //   await mongoose.connect('mongodb+srv://arshadraza:arshad7862@cluster0.lrpglgh.mongodb.net/portfolio');
+        // await mongoose.connect('mongodb://127.0.0.1:27017/portfolio');
+        await mongoose.connect('mongodb+srv://arshadraza:arshad7862@cluster0.lrpglgh.mongodb.net/portfolio');
         console.log("connected to mongodb");
     }
     catch(error){
